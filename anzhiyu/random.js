e2de2d2f0c12e2ebd9fce5f6061164b118c2e410
xiao <1917435878@qq.com>
2025-09-06 23:18:43 +0800

    Site updated: 2025-09-06 23:18:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/22/hello-world/"];function toRandomPost(){
+var posts=["2025/08/22/hello-world/","2025/08/27/项目一Linux网络连接-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
