e3d244ed95d21de79d368902afb9afb48efa26e2
xiao <1917435878@qq.com>
2025-10-09 16:41:13 +0800

    Site updated: 2025-10-09 16:41:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/22/hello-world/","2025/08/27/项目一Linux网络连接-0/","2025/10/03/任务1：区块链系统部署与运维/"];function toRandomPost(){
+var posts=["2025/08/22/hello-world/","2025/10/03/任务1：区块链系统部署与运维/","2025/08/27/项目一Linux网络连接-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
